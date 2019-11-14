71e64ec2107354f11ffa36caf7370fa8f6817bea
jack-huang2016 <2456120975@qq.com>
2019-11-14 16:55:25 +0800

    Springboot整合事务

---
+++ springboot-transaction-web/src/main/java/com/sample/test/SpringbootTransactionWebApplication.java
@@ -0,0 +1,13 @@
+package com.sample.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTransactionWebApplication.class, args);
+    }
+
+}
